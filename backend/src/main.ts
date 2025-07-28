bc7d70b07a26de551e488e47de33a203bb2db984
Yobra28 <briankuruui3768@gmail.com>
2025-07-28 17:10:38 +0300

    fixed backend cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,9 +8,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
-    credentials: true,
-  });
+  origin: ['http://localhost:4200', 'https://send-it-courier.vercel.app'],
+  credentials: true,
+});
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
   await app.listen(3000);
