f0f7ca9ac321823ae795534d7471c850d9a72ad2
Yobra28 <briankuruui3768@gmail.com>
2025-07-29 12:50:42 +0300

    removed the use of .env in frontend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: process.env['NG_APP_API_URL'] || 'https://sendit-courier-7847.onrender.com/api'
+  production: false,
+  apiUrl: 'https://sendit-courier-7847.onrender.com/api'
 };
 
