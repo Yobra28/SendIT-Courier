57dffd3cd1a1ae1b3775bace510cb0f5fca01f3e
Yobra28 <briankuruui3768@gmail.com>
2025-07-29 09:31:26 +0300

    fixed the frontend cors

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,3 +2,8 @@
 //   production: false,
 //   apiUrl: 'http://localhost:3000/api'
 // }; 
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://sendit-courier-7847.onrender.com/api'
+}; 
